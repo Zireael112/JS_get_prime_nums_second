96b7fdb934f0b7c37b3579bb89c2fa6c9af2c551
Vlad <kirito_22223@mail.ru>
2022-12-24 08:21:25 +0300

    main commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function getPrimeNums(n) {
+  const numbers = []
+  for (let i = 2; numbers.length < n; i++) {
+      let flag = false
+      for (let j = 0; j < i; j++) {
+          if (i % numbers[j] === 0) {
+              flag = true;
+              break;
+          }
+      }
+      if (flag === false) {
+          numbers.push(i)
+      }
+  }
+  return console.log(numbers)
+}
+
+getPrimeNums(process.argv[2])
